0ecbdbd56c5b9f0ef31dfb2d590e0725b43403e6
Osspial <osspial@gmail.com>
2016-08-19 13:30:10 -0400

    Added shell drawing api

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,25 @@
 extern crate gl;
 extern crate gl_raii;
+
+pub trait Drawable {
+    type Foreground: Drawable;
+    type Fill: Drawable;
+    type Backdrop: Drawable;
+
+    fn rect(&self) -> Rect;
+    fn mask(&self) -> Mask;
+    fn border(&self) -> Border;
+    fn foreground(&self) -> Self::Foreground;
+    fn fill(&self) -> Self::Fill;
+    fn backdrop(&self) -> Self::Backdrop;
+
+    fn draw<S: Surface>(&self, &S);
+}
+
+pub struct Rect {}
+
+pub struct Mask {}
+
+pub struct Border {}
+
+pub trait Surface {}
