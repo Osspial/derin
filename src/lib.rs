63cb0ec2dbd7ccabb4fb43cac315628a818a3f70
Osspial <osspial@gmail.com>
2016-08-19 13:36:17 -0400

    Began fleshing out Rect

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,13 @@ pub trait Drawable {
     type Fill: Drawable;
     type Backdrop: Drawable;
 
-    fn rect(&self) -> Rect;
+    fn rect(&self) -> Rect {
+        Rect::new(
+            Point::new(-1.0,  1.0),
+            Point::new( 1.0, -1.0)
+        )
+    }
+
     fn mask(&self) -> Mask;
     fn border(&self) -> Border;
     fn foreground(&self) -> Self::Foreground;
@@ -16,10 +22,40 @@ pub trait Drawable {
     fn draw<S: Surface>(&self, &S);
 }
 
-pub struct Rect {}
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Rect {
+    /// Upper-left corner of rectangle
+    pub ul: Point,
+    // Lower-right corner of rectangle
+    pub lr: Point
+}
+
+impl Rect {
+    pub fn new(ul: Point, lr: Point) -> Rect {
+        Rect {
+            ul: ul,
+            lr: lr
+        }
+    }
+}
 
 pub struct Mask {}
 
 pub struct Border {}
 
 pub trait Surface {}
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Point {
+    pub x: f32,
+    pub y: f32
+}
+
+impl Point {
+    pub fn new(x: f32, y: f32) -> Point {
+        Point {
+            x: x,
+            y: y
+        }
+    }
+}
