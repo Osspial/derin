f3067e02060f25050e1483c4f149b5c4ea23f1a8
Osspial <osspial@gmail.com>
2016-08-21 18:17:37 -0400

    Modified draw to use internal, complex point

--- src/draw/mod.rs
+++ src/draw/mod.rs
@@ -1,4 +1,4 @@
-use cgmath::{Point2, Vector2};
+use cgmath::{Vector2};
 
 pub trait Drawable<C: Composite = ()> {
     fn shader_data<'a>(&'a self) -> Shader<'a, C>;
@@ -6,7 +6,7 @@ pub trait Drawable<C: Composite = ()> {
 
 pub enum Shader<'a, C: Composite> {
     Verts {
-        vertices: &'a [Vertex],
+        verts: &'a [Vertex],
         indices: &'a [u16]
     },
 
@@ -21,10 +21,22 @@ pub enum Shader<'a, C: Composite> {
     None
 }
 
+#[derive(Debug, Clone, Copy)]
 pub struct Vertex {
-    pub pos: Point2<f32>,
+    pub pos: Point,
     pub normal: Vector2<f32>,
     pub color: Color
+}
+
+impl Vertex {
+    #[inline]
+    pub fn new(pos: Point, normal: Vector2<f32>, color: Color) -> Vertex {
+        Vertex {
+            pos: pos,
+            normal: normal,
+            color: color
+        }
+    }
 }
 
 pub trait Composite: Drawable<Self> 
@@ -75,30 +87,91 @@ impl Composite for () {
     fn backdrop(&self) {}
 }
 
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Complex {
+    pub rel: f32,
+    pub abs: f32
+}
+
+impl Complex {
+    pub fn new(rel: f32, abs: f32) -> Complex {
+        Complex {
+            rel: rel,
+            abs: abs
+        }
+    }
+
+    pub fn new_rel(rel: f32) -> Complex {
+        Complex {
+            rel: rel,
+            abs: 0.0
+        }
+    }
+
+    pub fn new_abs(abs: f32) -> Complex {
+        Complex {
+            rel: 0.0,
+            abs: abs
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Point {
+    pub x: Complex,
+    pub y: Complex
+}
+
+impl Point {
+    pub fn new(x: Complex, y: Complex) -> Point {
+        Point {
+            x: x,
+            y: y
+        }
+    }
+}
 
 #[derive(Debug, Clone, Copy)]
 pub struct Rect {
     /// Upper-left corner of rectangle
-    pub ul: Point2<f32>,
+    pub upleft: Point,
     // Lower-right corner of rectangle
-    pub lr: Point2<f32>
+    pub lowright: Point
+}
+
+impl Rect {
+    pub fn new(upleft: Point, lowright: Point) -> Rect {
+        Rect {
+            upleft: upleft,
+            lowright: lowright
+        }
+    }
+
+    /// Calculate upper-right corner of rectangle
+    pub fn upright(self) -> Point {
+        Point {
+            x: self.upleft.x,
+            y: self.lowright.y
+        }
+    }
+
+    /// Calculate lower-left corner of rectangle
+    pub fn lowleft(self) -> Point {
+        Point {
+            x: self.lowright.x,
+            y: self.upleft.y
+        }
+    }
 }
 
 impl Default for Rect {
     fn default() -> Rect {
+        let one = Complex::new_rel(1.0);
+        let none = Complex::new_rel(-1.0);
         Rect::new(
-            Point2::new(-1.0,  1.0),
-            Point2::new( 1.0, -1.0)
+            Point::new(none,  one),
+            Point::new( one, none)
         )
-    }
-}
-
-impl Rect {
-    pub fn new(ul: Point2<f32>, lr: Point2<f32>) -> Rect {
-        Rect {
-            ul: ul,
-            lr: lr
-        }
     }
 }
 
