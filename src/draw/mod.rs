dae7cc1ec08fb3db89c05e87d8f39d45da7266f0
Osspial <osspial@gmail.com>
2016-08-21 22:59:48 -0400

    Made Complex contain Points instead of Points containing Complexes

--- src/draw/mod.rs
+++ src/draw/mod.rs
@@ -77,28 +77,28 @@ impl Default for Border {
 
 #[derive(Debug, Clone, Copy, Default)]
 pub struct Complex {
-    pub rel: f32,
-    pub abs: f32
+    pub rel: Point,
+    pub abs: Point
 }
 
 impl Complex {
-    pub fn new(rel: f32, abs: f32) -> Complex {
+    pub fn new(rel: Point, abs: Point) -> Complex {
         Complex {
             rel: rel,
             abs: abs
         }
     }
 
-    pub fn new_rel(rel: f32) -> Complex {
+    pub fn new_rel(rel: Point) -> Complex {
         Complex {
             rel: rel,
-            abs: 0.0
+            abs: Point::new(0.0, 0.0)
         }
     }
 
-    pub fn new_abs(abs: f32) -> Complex {
+    pub fn new_abs(abs: Point) -> Complex {
         Complex {
-            rel: 0.0,
+            rel: Point::new(0.0, 0.0),
             abs: abs
         }
     }
@@ -106,12 +106,12 @@ impl Complex {
 
 #[derive(Debug, Clone, Copy, Default)]
 pub struct Point {
-    pub x: Complex,
-    pub y: Complex
+    pub x: f32,
+    pub y: f32
 }
 
 impl Point {
-    pub fn new(x: Complex, y: Complex) -> Point {
+    pub fn new(x: f32, y: f32) -> Point {
         Point {
             x: x,
             y: y
@@ -121,14 +121,14 @@ impl Point {
 
 #[derive(Debug, Clone, Copy)]
 pub struct Vertex {
-    pub pos: Point,
+    pub pos: Complex,
     pub normal: Vector2<f32>,
     pub color: Color
 }
 
 impl Vertex {
     #[inline]
-    pub fn new(pos: Point, normal: Vector2<f32>, color: Color) -> Vertex {
+    pub fn new(pos: Complex, normal: Vector2<f32>, color: Color) -> Vertex {
         Vertex {
             pos: pos,
             normal: normal,
@@ -140,13 +140,13 @@ impl Vertex {
 #[derive(Debug, Clone, Copy)]
 pub struct Rect {
     /// Upper-left corner of rectangle
-    pub upleft: Point,
+    pub upleft: Complex,
     // Lower-right corner of rectangle
-    pub lowright: Point
+    pub lowright: Complex
 }
 
 impl Rect {
-    pub fn new(upleft: Point, lowright: Point) -> Rect {
+    pub fn new(upleft: Complex, lowright: Complex) -> Rect {
         Rect {
             upleft: upleft,
             lowright: lowright
@@ -154,29 +154,27 @@ impl Rect {
     }
 
     /// Calculate upper-right corner of rectangle
-    pub fn upright(self) -> Point {
-        Point {
-            x: self.upleft.x,
-            y: self.lowright.y
+    pub fn upright(self) -> Complex {
+        Complex {
+            rel: Point::new(self.lowright.rel.x, self.upleft.rel.y),
+            abs: Point::new(self.lowright.abs.x, self.upleft.abs.y)
         }
     }
 
     /// Calculate lower-left corner of rectangle
-    pub fn lowleft(self) -> Point {
-        Point {
-            x: self.lowright.x,
-            y: self.upleft.y
+    pub fn lowleft(self) -> Complex {
+        Complex {
+            rel: Point::new(self.upleft.rel.x, self.lowright.rel.y),
+            abs: Point::new(self.upleft.abs.x, self.lowright.abs.y)
         }
     }
 }
 
 impl Default for Rect {
     fn default() -> Rect {
-        let one = Complex::new_rel(1.0);
-        let none = Complex::new_rel(-1.0);
         Rect::new(
-            Point::new(none,  one),
-            Point::new( one, none)
+            Complex::new_rel(Point::new(-1.0,  1.0)),
+            Complex::new_rel(Point::new( 1.0, -1.0))
         )
     }
 }
