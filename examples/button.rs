cd81d954b68a8bd14675ad23ff275aba81d7b97f
Osspial <osspial@gmail.com>
2016-08-23 00:08:54 -0400

    Added "button" example

---
+++ examples/button.rs
@@ -0,0 +1,63 @@
+extern crate tint;
+extern crate glutin;
+
+use tint::draw::{Drawable, Shadable, Shader, Surface, Color, Complex, Point, Rect};
+use tint::draw::primitive::ColorRect;
+use tint::draw::gl::{Facade, BufferData};
+
+struct DrawableRect {
+    rect: ColorRect,
+    buffers: BufferData
+}
+
+impl Shadable for DrawableRect {
+    fn shader_data<'a>(&'a self) -> Shader<'a, ()> {
+        self.rect.shader_data()
+    }
+
+    fn num_updates(&self) -> u64 {
+        self.rect.num_updates()
+    }
+}
+
+impl Drawable for DrawableRect {
+    fn buffer_data(&self) -> &BufferData {
+        &self.buffers
+    }
+}
+
+fn main() {
+    let window = glutin::WindowBuilder::new()
+        .with_dimensions(500, 500)
+        .with_pixel_format(24, 8)
+        .build().unwrap();
+
+    unsafe{ window.make_current().unwrap() };
+
+    let mut display = Facade::new(|s| window.get_proc_address(s) as *const _);
+
+    let rect = DrawableRect {
+        rect: ColorRect::new(
+            Color::new(255, 255, 255, 255), 
+            Rect::new(
+                Complex::new_rel(Point::new(-0.5,  0.5)),
+                Complex::new_rel(Point::new( 0.5, -0.5))
+            )
+        ),
+        buffers: BufferData::new()
+    };
+
+    'main: loop {
+        for event in window.poll_events() {
+            match event {
+                glutin::Event::Closed => break 'main,
+                _ => ()
+            }
+        }
+
+        let mut surface = display.surface();
+        surface.draw(&rect);
+
+        window.swap_buffers().unwrap();
+    }
+}
