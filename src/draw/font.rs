3fb45f56ec59a6a9343d9de9569c0ff461cbcbfe
Osspial <osspial@gmail.com>
2016-08-31 18:19:18 -0400

    Added normalized texture rect to CharDrawInfo, improved offset

--- src/draw/font.rs
+++ src/draw/font.rs
@@ -2,9 +2,10 @@ use freetype::{Library, RenderMode, Face, BitmapGlyph};
 use freetype::face::{LoadFlag, KerningMode};
 
 use std::path::PathBuf;
-use std::collections::HashSet;
-use std::iter::FromIterator;
+use std::collections::HashMap;
 use std::str::Chars;
+
+use std::ops::Mul;
 
 use super::Point;
 
@@ -36,12 +37,12 @@ thread_local!{
     static FT_LIB: Library = Library::init().unwrap();
 }
 
-struct BitmapIter<'a, I: Iterator<Item=char>> {
+struct BitmapIter<'a, I: Iterator<Item=&'a char>> {
     face: &'a Face<'static>,
     char_iter: I
 }
 
-impl<'a, I: Iterator<Item=char>> BitmapIter<'a, I> {
+impl<'a, I: Iterator<Item=&'a char>> BitmapIter<'a, I> {
     fn new(face: &'a Face<'static>, char_iter: I) -> BitmapIter<'a, I> {
         BitmapIter {
             face: face,
@@ -50,11 +51,12 @@ impl<'a, I: Iterator<Item=char>> BitmapIter<'a, I> {
     }
 }
 
-impl<'a, I: Iterator<Item=char>> Iterator for BitmapIter<'a, I> {
+impl<'a, I: Iterator<Item=&'a char>> Iterator for BitmapIter<'a, I> {
     type Item = BitmapGlyph;
 
     fn next(&mut self) -> Option<BitmapGlyph> {
         if let Some(c) = self.char_iter.next() {
+            let c = *c;
             let char_index = self.face.get_char_index(c as usize);
             self.face.load_glyph(char_index, LoadFlag::empty()).unwrap();
             let glyph = self.face.glyph().get_glyph().unwrap();
@@ -143,56 +145,81 @@ pub struct ImageRef<'a> {
     pub height: u32
 }
 
+const ASCII_CHAR_ARRAY: &'static [char] = 
+    &[' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/',
+      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?',
+      '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 
+      'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', 
+      '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
+      'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~'];
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+enum StyledChar {
+    Regular(char),
+    Italic(char),
+    Bold(char),
+    BoldItalic(char)
+}
+
+#[derive(Debug)]
+struct AtlasCharInfo {
+    center_offset: Point,
+    image_rect: ImageRect
+}
+
 pub struct FontAtlas {
     pixels: Vec<u8>,
     width: u32,
     height: u32,
-    charset: HashSet<char>,
+    charmap: HashMap<StyledChar, AtlasCharInfo>,
     font_sizes: Vec<u32>,
     dpi: u32
 }
 
 impl FontAtlas {
     fn new(dpi: u32) -> FontAtlas {
-        use std::char;
-
         FontAtlas {
             pixels: Vec::with_capacity(512 * 512),
             width: 0,
             height: 0,
-            charset: HashSet::from_iter((32..127).into_iter().map(|d| char::from_u32(d).unwrap())),
+            charmap: HashMap::new(),
             font_sizes: Vec::with_capacity(4),
             dpi: dpi
         }
     }
 
     fn recalculate_image(&mut self, faces: &FontFaces) {
+        use self::StyledChar::*;
+
         let width: u32 = 512;
         let mut height: u32 = 512;
 
-        let mut bitmaps: Vec<BitmapGlyph> = Vec::with_capacity(128);
+        let mut bitmaps: Vec<(StyledChar, BitmapGlyph)> = Vec::with_capacity(128);
+
+        self.charmap.clear();
 
         // Load the glyph bitmaps into the bitmap vector
         for size in self.font_sizes.iter().map(|s| *s) {
             faces.regular.set_char_size(0, size as isize * 64, self.dpi, self.dpi).unwrap();
-            bitmaps.extend(BitmapIter::new(&faces.regular, self.charset.iter().map(|c| *c)));
+
+            bitmaps.extend(ASCII_CHAR_ARRAY.iter().map(|c| Regular(*c)).zip(BitmapIter::new(&faces.regular, ASCII_CHAR_ARRAY.iter())));
 
             if let Some(ref italic) = faces.italic {
                 italic.set_char_size(0, size as isize * 64, self.dpi, self.dpi).unwrap();
-                bitmaps.extend(BitmapIter::new(italic, self.charset.iter().map(|c| *c)));
+                bitmaps.extend(ASCII_CHAR_ARRAY.iter().map(|c| Italic(*c)).zip(BitmapIter::new(italic, ASCII_CHAR_ARRAY.iter())));
             }
             if let Some(ref bold) = faces.bold {
                 bold.set_char_size(0, size as isize * 64, self.dpi, self.dpi).unwrap();
-                bitmaps.extend(BitmapIter::new(bold, self.charset.iter().map(|c| *c)));
+                bitmaps.extend(ASCII_CHAR_ARRAY.iter().map(|c| Bold(*c)).zip(BitmapIter::new(bold, ASCII_CHAR_ARRAY.iter())));
             }
             if let Some(ref bold_italic) = faces.bold_italic {
                 bold_italic.set_char_size(0, size as isize * 64, self.dpi, self.dpi).unwrap();
-                bitmaps.extend(BitmapIter::new(bold_italic, self.charset.iter().map(|c| *c)));
+                bitmaps.extend(ASCII_CHAR_ARRAY.iter().map(|c| BoldItalic(*c)).zip(BitmapIter::new(bold_italic, ASCII_CHAR_ARRAY.iter())));
             }
         }
 
         // Sort the bitmap vector by descending height
-        bitmaps.sort_by_key(|b| -(b.bitmap().rows() as isize));
+        bitmaps.sort_by_key(|b| -(b.1.bitmap().rows() as isize));
 
         // If the currently-allocated pixels vector does not have the capacity for a font atlas with
         // the specified width and height, allocate a new vector. Otherwise, just re-use the current
@@ -210,7 +237,9 @@ impl FontAtlas {
         // The x pixel location where we're going to be writing to the atlas image.
         let mut cursor: u32 = 0;
 
-        for b in &bitmaps {
+        for &(c, b) in &bitmaps {
+            let b_left = b.left();
+            let b_top = b.top();
             let b = b.bitmap();
 
             if cursor + b.width() as u32 > width {
@@ -220,9 +249,9 @@ impl FontAtlas {
             let cursorus = cursor as usize;
 
             if 0 < b.width() {
-                for y in (0..b.rows()).map(|y| y as u32) {
-                    let max_height_in_range = heights[cursorus..cursorus + b.width() as usize].iter().max().unwrap();
+                let max_height_in_range = *heights[cursorus..cursorus + b.width() as usize].iter().max().unwrap();
 
+                for y in (0..b.rows()).map(|y| y as u32) {
                     let pixels_index = (y * width + cursor + max_height_in_range * width) as usize;
                     let bitmap_index = (y * b.width() as u32) as usize;
 
@@ -236,10 +265,25 @@ impl FontAtlas {
                         .copy_from_slice(&b.buffer()[bitmap_index..bitmap_index + b.width() as usize]);
                 }
 
+                // Initially, the rect has pixels, not normalized texture coordinates. This is because we don't
+                // know if the image's current resolution will actually be its final resolution. We convert these
+                // into normalized texture coordinates after every glyph has been loaded into the image.
+                let pixel_rect = ImageRect {
+                    upleft: Point::new(cursor as f32, max_height_in_range as f32),
+                    lowright: Point::new(cursor as f32 + b.width() as f32, max_height_in_range as f32 + b.rows() as f32)
+                };
+
+                let char_info = AtlasCharInfo {
+                    center_offset: px_to_pts(self.dpi, Point::new(b_left as f32, b_top as f32)),
+                    image_rect: pixel_rect
+                };
+
+                self.charmap.insert(c, char_info);
+
                 let heights_slice = if cursor + b.width() as u32 > width {
                     &mut heights[cursorus..]
                 } else {
-                    &mut heights[cursorus..cursorus+ b.width() as usize]
+                    &mut heights[cursorus..cursorus + b.width() as usize]
                 };
 
                 let new_height = heights_slice.iter().max().unwrap() + b.rows() as u32;
@@ -257,6 +301,15 @@ impl FontAtlas {
 
         height = *heights.iter().max().unwrap();
         self.pixels.truncate((height * width) as usize);
+
+        // Because the final dimensions of the image are now known, we can safely convert the glyph rects from
+        // pixel coordinates to normalized texture coordinates.
+        for (_, aci) in self.charmap.iter_mut() {
+            aci.image_rect.upleft.x /= width as f32;
+            aci.image_rect.upleft.y /= height as f32;
+            aci.image_rect.lowright.x /= width as f32;
+            aci.image_rect.lowright.y /= height as f32;
+        }
 
         self.width = width;
         self.height = height;
@@ -279,10 +332,11 @@ impl<'a, 's> Iterator for CharDrawInfoIter<'a, 's> {
 
     fn next(&mut self) -> Option<CharDrawInfo> {
         if let Some(c) = self.chars.next() {
+            let atlas_char_info = self.font.atlas.charmap.get(&StyledChar::Regular(c)).unwrap();
+
             let last_char_index = self.font.faces.regular.get_char_index(self.last_char as usize);
             let char_index = self.font.faces.regular.get_char_index(c as usize);
             self.font.faces.regular.load_glyph(char_index, LoadFlag::empty()).expect("Could not load glyph");
-            let dpi_scale = 1.0 / (self.dpi as f32 / 72.0);
 
             if self.has_kerning {
                 let kerning = self.font.faces.regular
@@ -299,7 +353,8 @@ impl<'a, 's> Iterator for CharDrawInfoIter<'a, 's> {
 
             Some(CharDrawInfo{
                 character: c,
-                offset: (self.offset / 64.0) * dpi_scale
+                image_rect: atlas_char_info.image_rect,
+                offset: px_to_pts(self.dpi, self.offset / 64.0) + atlas_char_info.center_offset
             })
         } else {
             None
@@ -307,15 +362,21 @@ impl<'a, 's> Iterator for CharDrawInfoIter<'a, 's> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct CharDrawInfo {
     pub character: char,
-    // pub rect: ImageRect,
+    pub image_rect: ImageRect,
     /// The number of points on the xy plane offset from the first character in the string
     pub offset: Point
 }
 
+#[derive(Debug, Clone, Copy)]
 pub struct ImageRect {
     pub upleft: Point,
     pub lowright: Point
+}
+
+fn px_to_pts<P: Mul<f32>>(dpi: u32, pixels: P) -> <P as Mul<f32>>::Output {
+    let dpi_scale = 1.0 / (dpi as f32 / 72.0);
+    pixels * dpi_scale
 }
