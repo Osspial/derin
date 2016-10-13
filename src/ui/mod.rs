502c47359049f6b875a0d95d4d1f767083b85311
Osspial <osspial@gmail.com>
2016-10-13 17:39:33 -0400

    Removed Container trait in lieu of crawl_children on Node w/ TreeCralwer

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -13,25 +13,24 @@ pub enum MouseButton {
 
 #[derive(Debug, Clone, Copy)]
 pub struct NodeMeta<'a> {
-    pub typ: &'a str,
-    pub id: Option<&'a str>,
+    pub ty: &'a str,
+    pub ident: Option<&'a str>,
+    pub state: Option<&'a str>,
     pub class: Option<&'a str>
 }
 
-pub struct Selector {}
+pub trait TreeCrawler {
+    fn add_node<N: Node>(&mut self, N);
+    fn add_control<C: Control>(&mut self, C);
+}
 
 pub trait Node {
     fn metadata(&self) -> NodeMeta;
+    fn crawl_children<T: TreeCrawler>(&self, T);
 }
 
 pub trait Control: Node {
     type Action;
 
     fn on_mouse_event(&mut self, MouseEvent) -> Self::Action;
-}
-
-pub trait Container: Node {
-    type Action;
-
-    fn get_control(&self, Selector) -> Option<&Control<Action = Self::Action>>;
 }
