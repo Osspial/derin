2d6dd316aef166fd2a62d0b6b0742ee6b8073c96
Osspial <osspial@gmail.com>
2016-10-24 18:46:52 -0400

    Added basic top-level API implementation for UI tree crawler

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -10,35 +10,121 @@ pub enum MouseButton {
     Other(u8)
 }
 
-pub struct Text {}
+/// The general data processor through which the UI-crawling state machine sends data.
+struct DataProcessor {}
+impl DataProcessor {
+    fn push_node(&mut self, _: NodeLink) {unimplemented!()}
+    fn push_image_node(&mut self, _: NodeLink, _: &Image) {unimplemented!()}
+    fn push_text_node(&mut self, _: NodeLink, _: &Text) {unimplemented!()}
+    fn call_control<C: Control>(&mut self, _: &mut C) {unimplemented!()}
+}
+
 pub struct Image {}
+pub struct Text {}
 
-pub struct NodeDataCollector {}
-pub struct ChildDataCollector {}
-pub struct ContentCollector {}
-
-impl NodeDataCollector {
-    pub fn node_type(&mut self, _: &str) {unimplemented!()}
-    pub fn node_state(&mut self, _: &str) {unimplemented!()}
-    pub fn node_class(&mut self, _: &str) {unimplemented!()}
-
-    pub fn node_control<C: Control>(&mut self, _: &mut C) {unimplemented!()}
-
-    pub fn children(self) -> ChildDataCollector {unimplemented!()}
-    pub fn contents(self) -> ContentCollector {unimplemented!()}
+#[derive(Debug, Clone, Copy)]
+struct NodeData<'a> {
+    name: &'a str,
+    ty: Option<&'a str>,
+    state: Option<&'a str>,
 }
 
-impl ChildDataCollector {
-    pub fn take(&mut self, _: &str) -> NodeDataCollector {unimplemented!()}
+impl<'a> NodeData<'a> {
+    fn new(name: &'a str) -> NodeData<'a> {
+        NodeData {
+            name: name,
+            ty: None,
+            state: None
+        }
+    }
 }
 
-impl ContentCollector {
-    pub fn push_image(&mut self, _: Image) {unimplemented!()}
-    pub fn push_text(&mut self, _: Text) {unimplemented!()}
+#[derive(Debug, Clone, Copy)]
+struct NodeLink<'l, 'a: 'l> {
+    last_node: Option<&'l NodeLink<'l, 'a>>,
+    node: NodeData<'a>
+}
+
+
+// These three structs represent the state machine used to crawl a UI tree. Really, they mainly
+// exist as an abstraction layer over the `DataProcessor` struct, where all the real processing
+// happens. They're also zero-cost, which is nice.
+
+pub struct NodeProcessor<'a> {
+    link: NodeLink<'a, 'static>,
+    proccer: &'a mut DataProcessor
+}
+
+pub struct ChildProcessor<'a> {
+    link: NodeLink<'a, 'static>,
+    proccer: &'a mut DataProcessor
+}
+
+pub struct ContentProcessor<'a> {
+    link: NodeLink<'a, 'static>,
+    proccer: &'a mut DataProcessor
+}
+
+
+impl<'a> NodeProcessor<'a> {
+    pub fn node_type(&mut self, node_type: &'static str) {
+        self.link.node.ty = Some(node_type);
+    }
+
+    pub fn node_state(&mut self, node_state: &'static str) {
+        self.link.node.state = Some(node_state);
+    }
+
+
+    pub fn node_control<C: Control>(&mut self, control: &mut C) {
+        self.proccer.call_control(control);
+    }
+
+    pub fn children(self) -> ChildProcessor<'a> {
+        ChildProcessor {
+            link: self.link,
+            proccer: self.proccer
+        }
+    }
+
+    pub fn contents(self) -> ContentProcessor<'a> {
+        ContentProcessor {
+            link: self.link,
+            proccer: self.proccer
+        }
+    }
+}
+
+impl<'a> Drop for NodeProcessor<'a> {
+    fn drop(&mut self) {
+        self.proccer.push_node(self.link)
+    }
+}
+
+impl<'a> ChildProcessor<'a> {
+    pub fn take(&mut self, name: &'static str) -> NodeProcessor {
+        NodeProcessor {
+            link: NodeLink {
+                last_node: Some(&self.link),
+                node: NodeData::new(name)
+            },
+            proccer: self.proccer
+        }
+    }
+}
+
+impl<'a> ContentProcessor<'a> {
+    pub fn push_image(self, image: &Image) {
+        self.proccer.push_image_node(self.link, image);
+    }
+
+    pub fn push_text(self, text: &Text) {
+        self.proccer.push_text_node(self.link, text);
+    }
 }
 
 pub trait Node {
-    fn node_data(&mut self, NodeDataCollector);
+    fn node_data(&mut self, NodeProcessor);
 }
 
 pub trait Control {
