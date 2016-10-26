261e504e23025e5a258d29c2273c1792c7f6d70b
Osspial <osspial@gmail.com>
2016-10-25 21:38:57 -0400

    Replaced UI module API with more flexible specialization-based system

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -10,124 +10,27 @@ pub enum MouseButton {
     Other(u8)
 }
 
-/// The general data processor through which the UI-crawling state machine sends data.
-struct DataProcessor {}
-impl DataProcessor {
-    fn push_node(&mut self, _: NodeLink) {unimplemented!()}
-    fn push_image_node(&mut self, _: NodeLink, _: &Image) {unimplemented!()}
-    fn push_text_node(&mut self, _: NodeLink, _: &Text) {unimplemented!()}
-    fn call_control<C: Control>(&mut self, _: &mut C) {unimplemented!()}
-}
-
-pub struct Image {}
-pub struct Text {}
-
-#[derive(Debug, Clone, Copy)]
-struct NodeData<'a> {
-    name: &'a str,
-    ty: Option<&'a str>,
-    state: Option<&'a str>,
-}
-
-impl<'a> NodeData<'a> {
-    fn new(name: &'a str) -> NodeData<'a> {
-        NodeData {
-            name: name,
-            ty: None,
-            state: None
-        }
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-struct NodeLink<'l, 'a: 'l> {
-    last_node: Option<&'l NodeLink<'l, 'a>>,
-    node: NodeData<'a>
-}
-
-
-// These three structs represent the state machine used to crawl a UI tree. Really, they mainly
-// exist as an abstraction layer over the `DataProcessor` struct, where all the real processing
-// happens. They're also zero-cost, which is nice.
-
-pub struct NodeProcessor<'a> {
-    link: NodeLink<'a, 'static>,
-    proccer: &'a mut DataProcessor
-}
-
-pub struct ChildProcessor<'a> {
-    link: NodeLink<'a, 'static>,
-    proccer: &'a mut DataProcessor
-}
-
-pub struct ContentProcessor<'a> {
-    link: NodeLink<'a, 'static>,
-    proccer: &'a mut DataProcessor
-}
-
-
-impl<'a> NodeProcessor<'a> {
-    pub fn node_type(&mut self, node_type: &'static str) {
-        self.link.node.ty = Some(node_type);
-    }
-
-    pub fn node_state(&mut self, node_state: &'static str) {
-        self.link.node.state = Some(node_state);
-    }
-
-
-    pub fn node_control<C: Control>(&mut self, control: &mut C) {
-        self.proccer.call_control(control);
-    }
-
-    pub fn children(self) -> ChildProcessor<'a> {
-        ChildProcessor {
-            link: self.link,
-            proccer: self.proccer
-        }
-    }
-
-    pub fn contents(self) -> ContentProcessor<'a> {
-        ContentProcessor {
-            link: self.link,
-            proccer: self.proccer
-        }
-    }
-}
-
-impl<'a> Drop for NodeProcessor<'a> {
-    fn drop(&mut self) {
-        self.proccer.push_node(self.link)
-    }
-}
-
-impl<'a> ChildProcessor<'a> {
-    pub fn take(&mut self, name: &'static str) -> NodeProcessor {
-        NodeProcessor {
-            link: NodeLink {
-                last_node: Some(&self.link),
-                node: NodeData::new(name)
-            },
-            proccer: self.proccer
-        }
-    }
-}
-
-impl<'a> ContentProcessor<'a> {
-    pub fn push_image(self, image: &Image) {
-        self.proccer.push_image_node(self.link, image);
-    }
-
-    pub fn push_text(self, text: &Text) {
-        self.proccer.push_text_node(self.link, text);
-    }
+/// The trait implemented for a type that processes nodes. This trait definition might seem a bit
+/// strange to you on first glance, as `N` is in the trait definition and not the `add_child`
+/// function. The reason for this is specialization. By having `N` outside of `add_child`,
+/// implementors of NodeProcessor have a much easier time implementing intrinsics (such as images
+/// and text), can define custom intrinsics, extend UI functionality with additional traits, and
+/// add type-specific optimizations, all due to specialization. Of course, at this point
+/// specialization isn't stable, limiting this library to nightly, but the increases in
+/// implementation ergonomics and flexability are well worth it.
+pub trait NodeProcessor<'a, N: Node + ?Sized> {
+    fn add_child(&mut self, name: &'static str, node: &mut N);
 }
 
 pub trait Node {
-    fn node_data(&mut self, NodeProcessor);
+    fn type_name() -> &'static str;
 }
 
-pub trait Control {
+pub trait ParentNode: Node {
+    fn children<'a, N>(&mut self, N) where N: NodeProcessor<'a, Self>;
+}
+
+pub trait Control: Node {
     type Action;
 
     fn on_mouse_event(&mut self, MouseEvent) -> Option<Self::Action> {None}
