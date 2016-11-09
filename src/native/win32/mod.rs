51207812748d1580b753e6cdae73c8219e1f864a
Osspial <osspial@gmail.com>
2016-11-09 17:31:14 -0500

    Improved comments and documentation in win32 module

--- src/native/win32/mod.rs
+++ src/native/win32/mod.rs
@@ -29,10 +29,14 @@ impl<N: Node> Window<N> {
         // Channel for the handle to the window
         let (window_sender, window_receiver) = mpsc::channel();
 
+        // Spawn a child thread in which UI windows are created. Messages are sent to this thread via the
+        // `win32` `SendMessageW` function, and window handles and actions are sent back to the main thread
+        // via Rust's MPSC channels.
         thread::spawn(move || {
             unsafe {
+                // Create a wrapper toplevel window. If the creation succeeds, send back the window. Otherwise, send
+                // back the error it created and terminate this thread.
                 let wrapper_window = Toplevel::new(&config);
-
                 match wrapper_window {
                     Ok(wr) => {
                         window_sender.send(Ok(WindowNode::Toplevel(wr))).unwrap();
@@ -44,6 +48,7 @@ impl<N: Node> Window<N> {
                     }
                 }
                 
+                // Populate the thread-local callback data with the data needed for the `win32` callback.
                 CALLBACK_DATA.with(|cd| {
                     let mut cd = cd.borrow_mut();
                     *cd = Some(CallbackData {
@@ -60,12 +65,15 @@ impl<N: Node> Window<N> {
             }
         });
 
+        // Receive the top-level window from the child thread. We know that we're going to get SOMEthing at
+        // the very least, so we can unwrap the `recv()` call. However, we might receive an error in which
+        // case that gets propagated.
         let wrapper_window = window_receiver.recv().unwrap()?;
 
         Ok(
             Window {
                 root: root,
-                // The toplevel node, this is a top-level window which contains all other nodes.
+                // The node that contains the top-level node and all other nodes, including root.
                 node_tree_root: NodeTreeBranch {
                     state_id: 0,
                     name: "toplevel",
@@ -96,7 +104,8 @@ struct NodeTreeBranch {
     children: Vec<NodeTreeBranch>
 }
 
-trait IntoNTB {
+/// Trait for converting `Node`s into `NodeTreeBranch`es.
+trait IntoNTB: Node {
     fn into_ntb(&self, name: &'static str, parent: &WindowNode, receiver: &Receiver<NativeResult<WindowNode>>) -> NativeResult<NodeTreeBranch>;
 }
 
@@ -112,7 +121,10 @@ impl<N: Node> IntoNTB for N {
 }
 
 struct NodeTraverser<'a> {
+    /// The branch that this instance of NodeTraverser is currently processing
     node_branch: &'a mut NodeTreeBranch,
+    /// The `WindowNode` belonging to the parent `NodeTreeBranch` of this `NodeTraverser`. In the event
+    /// that this `NodeTraverser` represents the root node, this is `None`.
     parent_window: Option<&'a WindowNode>,
     receiver: &'a Receiver<NativeResult<WindowNode>>,
     /// The index in the child vector to first look at when searching for a child. As new
@@ -130,11 +142,18 @@ impl<'a> NodeTraverser<'a> {
         }
     }
 
+    /// Take a name, a node, and a function to run upon updating the node and either add the node to the
+    /// node tree (which runs the function as well) or determine whether or not to run the update function
+    /// and run it if necessary.
     fn process_node_child<N, PF>(&'a mut self, name: &'static str, node: &mut N, mut proc_func: PF)
             -> NativeResult<()>
             where N: Node,
                   PF: FnMut(&mut N, NodeTraverser<'a>) -> NativeResult<()>
     {
+        // If the desired node branch is at the current child index, get that and run the contents of the
+        // `if` statement. Otherwise, search the entire children vector for the desired node and run the
+        // `if` statement if that's found. If both of those fail, insert a new node branch and run the
+        // necessary processing function.
         if let Some(i) = self.node_branch.children.get(self.child_index)
                              .and_then(|branch| (branch.name == name).as_some(self.child_index))
                              .or(self.node_branch.children
@@ -143,6 +162,8 @@ impl<'a> NodeTraverser<'a> {
                                     .next()) {
             self.child_index = i + 1;
 
+            // Compare the newly-generated state id and the cached state id. If there is a mismatch, update the
+            // cached id and run the processing function.
             let new_state_id = node.state_id();
             if self.node_branch.children[i].state_id != new_state_id {
                 self.node_branch.children[i].state_id = new_state_id;
