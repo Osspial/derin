a1500d9d087fe6b60bf01d29a1ad11b40e61a159
Osspial <osspial@gmail.com>
2016-11-11 14:58:46 -0500

    Moved gl renderer out of project

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,10 @@
 #![feature(specialization, conservative_impl_trait)]
 
-extern crate fnv;
 extern crate rand;
-#[macro_use]
-extern crate bitflags;
 extern crate boolinator;
 #[macro_use]
 extern crate lazy_static;
 extern crate smallvec;
-
-#[cfg(feature="gl_ui")]
-extern crate gl;
-#[cfg(feature="gl_ui")]
-extern crate gl_raii;
-#[cfg(feature="gl_ui")]
-extern crate cgmath;
-#[cfg(feature="gl_ui")]
-extern crate freetype;
-#[cfg(feature="gl_ui")]
-extern crate glutin;
 
 #[cfg(target_os="windows")]
 extern crate user32;
@@ -29,17 +15,5 @@ extern crate dwmapi;
 #[cfg(target_os="windows")]
 extern crate winapi;
 
-#[cfg(feature="gl_ui")]
-pub mod draw;
 pub mod native;
 pub mod ui;
-
-
-#[cfg(feature="gl_ui")]
-static mut ID_COUNTER: u64 = 0;
-#[cfg(feature="gl_ui")]
-fn get_unique_id() -> u64 {
-    let id = unsafe{ ID_COUNTER };
-    unsafe{ ID_COUNTER += 1 };
-    id
-}
--- src/shaders/color_passthrough.frag
+++
@@ -1,8 +0,0 @@
-#version 330
-
-in vec4 vert_color;
-out vec4 frag_color;
-
-void main() {
-    frag_color = vert_color;
-}
