b1d59ad62d5b9a73e739d01ebb7bbee1350ac7df
Osspial <osspial@gmail.com>
2017-03-04 16:21:51 -0500

    Remove several unused crates from derin root

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,12 @@
 #![feature(specialization, conservative_impl_trait)]
 
 extern crate rand;
-extern crate boolinator;
-#[macro_use]
-extern crate lazy_static;
-extern crate smallvec;
 extern crate dle;
 extern crate dct;
 
 #[cfg(test)]
 #[cfg_attr(test, macro_use)]
 extern crate quickcheck;
-
-#[cfg(target_os="windows")]
-extern crate user32;
-#[cfg(target_os="windows")]
-extern crate kernel32;
-#[cfg(target_os="windows")]
-extern crate dwmapi;
-#[cfg(target_os="windows")]
-extern crate winapi;
-#[cfg(target_os="windows")]
-extern crate comctl32;
 
 #[cfg(target_os="windows")]
 extern crate dww;
