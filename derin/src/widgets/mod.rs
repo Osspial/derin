874ab68b53f3ade158e3aaf56b03478eba77f8d6
Osspial <osspial@gmail.com>
2018-04-17 00:00:23 -0400

    Re-export core::popup from widgets::custom

--- derin/src/widgets/mod.rs
+++ derin/src/widgets/mod.rs
@@ -49,6 +49,7 @@ use core::render::Theme as CoreTheme;
 
 /// The `Widget` trait, as well as associated types used to create custom widgets.
 pub mod custom {
+    pub use core::popup;
     pub use core::timer::TimerRegister;
     pub use core::tree::{UpdateTag, Widget, WidgetSummary, WidgetIdent};
 }
