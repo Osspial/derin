476bb53c93436aeebd4cb843b96521730085cc89
Osspial <osspial@gmail.com>
2018-04-16 23:47:28 -0400

    Clear depth buffer when drawing

--- derin/src/gl_render/mod.rs
+++ derin/src/gl_render/mod.rs
@@ -252,6 +252,7 @@ impl Renderer for GLRenderer {
         let height_scaled = (height as f32 * scale_factor) as u32;
         self.frame.draw.render_state.viewport = DimsBox::new2(width_scaled, height_scaled).into();
         self.frame.draw.fb.clear_color(Rgba::new(1., 1., 1., 1.));
+        self.frame.draw.fb.clear_depth(0.0);
         self.frame.output_vertices = draw_output;
 
         (&mut self.frame, BoundBox::new2(0, 0, width as i32, height as i32))
