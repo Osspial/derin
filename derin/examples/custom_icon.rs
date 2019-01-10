0cd24e5ee20a37802bcf59175aa83b8a72933897
Osspial <osspial@gmail.com>
2019-01-09 23:15:54 -0500

    Make relayout not freeze program if root widget is smaller than min size

--- derin/examples/custom_icon.rs
+++ derin/examples/custom_icon.rs
@@ -58,7 +58,7 @@ fn main() {
     };
 
     let mut window = unsafe{ Window::new(window_config, group, theme).unwrap() };
-    let _: Option<()> = window.run_forever(
+    window.run_forever(
         |_: (), _, _| {
             LoopFlow::Continue
         },
